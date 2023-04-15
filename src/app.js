d02713286c53d0584d6a0d4f33a30365ee399010
QueenB-31 <125220221+Queency31NSPPD@users.noreply.github.com>
2023-04-15 15:27:42 +0100

    added images and updated html and css, js contents

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ document.querySelector('.btn').addEventListener('click', function() {
         let name=prompt('What is your name?');
 
         if (name.length) {
-            alert('Thanks, ' + name + ' , we will be in touch. Meanwhile, have a lot of Yogurt!')
+            alert('Thanks, ' + name + ' , we will be in touch. Meanwhile, have a lot of Ice-cream!')
         }
 
         else {
-            alert('No name, no Greek Yogurt for you!')
+            alert('No name, no Ice-cream for you!')
         }
     });
